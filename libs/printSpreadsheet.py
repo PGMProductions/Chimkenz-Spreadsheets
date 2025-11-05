872349e7309922f80672957ae805dac186f971fb
PGM Productions <bonamimarius@gmail.com>
2025-11-05 18:08:15 +0100

    first

---
+++ libs/printSpreadsheet.py
@@ -0,0 +1,17 @@
+from terminal import *  #¯\_(ツ)_/¯
+import os
+
+def printscreen(spreadsheet,originX,originY):
+    screenSize = os.get_terminal_size()
+    print(screenSize)
+
+def makeSquarePrintable(value,collumnSize):
+    """
+    ya give this the value of a square and your options and it gives you wat to print and how many lines
+    """
+    valueSTR = str(value)
+    
+    
+    
+    
+    
---
+++ libs/terminal.py
@@ -0,0 +1,46 @@
+#Credits to SandBuster for this file
+#https://github.com/sandbuster2005/ChimkenMuziks/blob/main/libs/handmade/terminal.py
+
+#I have no idea what this is
+
+import sys
+#pretty sure your not dumb if you see this so it can go without comment for now
+
+def out( text ):
+    sys.stdout.write( text )
+    sys.stdout.flush()
+
+def up( x = 1 ):
+    out( f'\x1b[{ x }A' )
+
+def lup( x = 1 ):
+    out( f'\x1b[{ x }F' )
+
+def down( x = 1):#dont work for now it seem
+    out ( f'\x1b[{ x }B' )
+
+def ldown( x = 1 ):
+    out( '\n'*x )
+
+def left( x = 1 ):
+    out( f'\x1b[{ x }D' )
+    
+def right( x = 1 ):
+    out( f'\x1b[{ x }C' )
+
+def home():
+    out( '\x1b[H' )
+
+def wipe():
+    out( '\x1b[2J' )
+    
+def wipe_line():
+    out( '\x1b[2K' )
+
+def save():
+    out( "\x1b7" )
+    
+def load():
+    out( "\x1b8" )
+
+    
