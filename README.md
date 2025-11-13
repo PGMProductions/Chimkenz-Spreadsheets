f9ba76264d1a182b4440e84b15607f626929cfc1
PGM Productions <bonamimarius@gmail.com>
2025-11-13 13:53:03 +0100

    commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Chimkenz-Spreadsheets
 Spreadsheet software from linux terminal
+
+
+
+with code from https://github.com/sandbuster2005/ChimkenMuziks
--- libs/printSpreadsheet.py
+++ libs/printSpreadsheet.py
@@ -1,7 +1,6 @@
 from terminal import background  #¯\_(ツ)_/¯
 import os
 import sys
-import baseconvert
 
 def printscreen(spreadsheet,originX,originY,collumnSize):
     """
@@ -16,13 +15,15 @@ def printscreen(spreadsheet,originX,originY,collumnSize):
     for line in range(originY,min(len(spreadsheet),originY+screenSize[1])):
         usefulLines.append(spreadsheet[line][originX:min(originX+screenSize[0],len(spreadsheet[line])-1)])
     
-    for line in range(len(usefulLines)):
-        printList.append(generateHeader(originX,collumnAmmount,collumnSize))
+    printList.append(generateHeader(originX,collumnAmmount,collumnSize))
+    for line in range(len(usefulLines)): 
         for termLine in makeLinePrintable(usefulLines[line],collumnSize,collumnAmmount,line):
             printList.append(termLine)
             
     for i in range(min(len(printList),screenSize[1])):
         print(printList[i])
+
+    print(generateSetBGColorString(0) + generateSetTextColorString(15))
 
 
 def makeSquarePrintable(value,collumnSize):
@@ -65,7 +66,14 @@ def makeLinePrintable(valuesList,collumnSize,collumnsAmmount,lineNumber):
 
 
 def generateHeader(originX,collumnAmmount,collumnSize):               #not made yet
-    return [background(255," "*collumnSize)] * collumnAmmount
+
+    headString = ""
+    for i in range(collumnAmmount):
+        headString = headString + " " + nameCollumn(i+originX)
+        headString = headString + (" " * (((i+1)*collumnSize) - len(headString)))     #this line adds as many spaces as neeeded to complete the square
+
+    return generateSetBGColorString(254) + generateSetTextColorString(0) + headString + generateSetTextColorString(15)
+
 
 def nameCollumn(collumn):
     alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","AA" , "AB" , "AC" , "AD" , "AE" , "AF" , "AG" , "AH" , "AI" , "AJ" , "AK" , "AL" , "AM" , "AN" , "AO" , "AP" , "AQ" , "AR" , "AS" , "AT" , "AU" , "AV" , "AW" , "AX" , "AY" , "AZ" , "BA" , "BB" , "BC" , "BD" , "BE" , "BF" , "BG" , "BH" , "BI" , "BJ" , "BK" , "BL" , "BM" , "BN" , "BO" , "BP" , "BQ" , "BR" , "BS" , "BT" , "BU" , "BV" , "BW" , "BX" , "BY" , "BZ" , "CA" , "CB" , "CC" , "CD" , "CE" , "CF" , "CG" , "CH" , "CI" , "CJ" , "CK" , "CL" , "CM" , "CN" , "CO" , "CP" , "CQ" , "CR" , "CS" , "CT" , "CU" , "CV" , "CW" , "CX" , "CY" , "CZ" , "DA" , "DB" , "DC" , "DD" , "DE" , "DF" , "DG" , "DH" , "DI" , "DJ" , "DK" , "DL" , "DM" , "DN" , "DO" , "DP" , "DQ" , "DR" , "DS" , "DT" , "DU" , "DV" , "DW" , "DX" , "DY" , "DZ" , "EA" , "EB" , "EC" , "ED" , "EE" , "EF" , "EG" , "EH" , "EI" , "EJ" , "EK" , "EL" , "EM" , "EN" , "EO" , "EP" , "EQ" , "ER" , "ES" , "ET" , "EU" , "EV" , "EW" , "EX" , "EY" , "EZ" , "FA" , "FB" , "FC" , "FD" , "FE" , "FF" , "FG" , "FH" , "FI" , "FJ" , "FK" , "FL" , "FM" , "FN" , "FO" , "FP" , "FQ" , "FR" , "FS" , "FT" , "FU" , "FV" , "FW" , "FX" , "FY" , "FZ" , "GA" , "GB" , "GC" , "GD" , "GE" , "GF" , "GG" , "GH" , "GI" , "GJ" , "GK" , "GL" , "GM" , "GN" , "GO" , "GP" , "GQ" , "GR" , "GS" , "GT" , "GU" , "GV" , "GW" , "GX" , "GY" , "GZ" , "HA" , "HB" , "HC" , "HD" , "HE" , "HF" , "HG" , "HH" , "HI" , "HJ" , "HK" , "HL" , "HM" , "HN" , "HO" , "HP" , "HQ" , "HR" , "HS" , "HT" , "HU" , "HV" , "HW" , "HX" , "HY" , "HZ" , "IA" , "IB" , "IC" , "ID" , "IE" , "IF" , "IG" , "IH" , "II" , "IJ" , "IK" , "IL" , "IM" , "IN" , "IO" , "IP" , "IQ" , "IR" , "IS" , "IT" , "IU" , "IV" , "IW" , "IX" , "IY" , "IZ" , "JA" , "JB" , "JC" , "JD" , "JE" , "JF" , "JG" , "JH" , "JI" , "JJ" , "JK" , "JL" , "JM" , "JN" , "JO" , "JP" , "JQ" , "JR" , "JS" , "JT" , "JU" , "JV" , "JW" , "JX" , "JY" , "JZ" , "KA" , "KB" , "KC" , "KD" , "KE" , "KF" , "KG" , "KH" , "KI" , "KJ" , "KK" , "KL" , "KM" , "KN" , "KO" , "KP" , "KQ" , "KR" , "KS" , "KT" , "KU" , "KV" , "KW" , "KX" , "KY" , "KZ" , "LA" , "LB" , "LC" , "LD" , "LE" , "LF" , "LG" , "LH" , "LI" , "LJ" , "LK" , "LL" , "LM" , "LN" , "LO" , "LP" , "LQ" , "LR" , "LS" , "LT" , "LU" , "LV" , "LW" , "LX" , "LY" , "LZ" , "MA" , "MB" , "MC" , "MD" , "ME" , "MF" , "MG" , "MH" , "MI" , "MJ" , "MK" , "ML" , "MM" , "MN" , "MO" , "MP" , "MQ" , "MR" , "MS" , "MT" , "MU" , "MV" , "MW" , "MX" , "MY" , "MZ" , "NA" , "NB" , "NC" , "ND" , "NE" , "NF" , "NG" , "NH" , "NI" , "NJ" , "NK" , "NL" , "NM" , "NN" , "NO" , "NP" , "NQ" , "NR" , "NS" , "NT" , "NU" , "NV" , "NW" , "NX" , "NY" , "NZ" , "OA" , "OB" , "OC" , "OD" , "OE" , "OF" , "OG" , "OH" , "OI" , "OJ" , "OK" , "OL" , "OM" , "ON" , "OO" , "OP" , "OQ" , "OR" , "OS" , "OT" , "OU" , "OV" , "OW" , "OX" , "OY" , "OZ" , "PA" , "PB" , "PC" , "PD" , "PE" , "PF" , "PG" , "PH" , "PI" , "PJ" , "PK" , "PL" , "PM" , "PN" , "PO" , "PP" , "PQ" , "PR" , "PS" , "PT" , "PU" , "PV" , "PW" , "PX" , "PY" , "PZ" , "QA" , "QB" , "QC" , "QD" , "QE" , "QF" , "QG" , "QH" , "QI" , "QJ" , "QK" , "QL" , "QM" , "QN" , "QO" , "QP" , "QQ" , "QR" , "QS" , "QT" , "QU" , "QV" , "QW" , "QX" , "QY" , "QZ" , "RA" , "RB" , "RC" , "RD" , "RE" , "RF" , "RG" , "RH" , "RI" , "RJ" , "RK" , "RL" , "RM" , "RN" , "RO" , "RP" , "RQ" , "RR" , "RS" , "RT" , "RU" , "RV" , "RW" , "RX" , "RY" , "RZ" , "SA" , "SB" , "SC" , "SD" , "SE" , "SF" , "SG" , "SH" , "SI" , "SJ" , "SK" , "SL" , "SM" , "SN" , "SO" , "SP" , "SQ" , "SR" , "SS" , "ST" , "SU" , "SV" , "SW" , "SX" , "SY" , "SZ" , "TA" , "TB" , "TC" , "TD" , "TE" , "TF" , "TG" , "TH" , "TI" , "TJ" , "TK" , "TL" , "TM" , "TN" , "TO" , "TP" , "TQ" , "TR" , "TS" , "TT" , "TU" , "TV" , "TW" , "TX" , "TY" , "TZ" , "UA" , "UB" , "UC" , "UD" , "UE" , "UF" , "UG" , "UH" , "UI" , "UJ" , "UK" , "UL" , "UM" , "UN" , "UO" , "UP" , "UQ" , "UR" , "US" , "UT" , "UU" , "UV" , "UW" , "UX" , "UY" , "UZ" , "VA" , "VB" , "VC" , "VD" , "VE" , "VF" , "VG" , "VH" , "VI" , "VJ" , "VK" , "VL" , "VM" , "VN" , "VO" , "VP" , "VQ" , "VR" , "VS" , "VT" , "VU" , "VV" , "VW" , "VX" , "VY" , "VZ" , "WA" , "WB" , "WC" , "WD" , "WE" , "WF" , "WG" , "WH" , "WI" , "WJ" , "WK" , "WL" , "WM" , "WN" , "WO" , "WP" , "WQ" , "WR" , "WS" , "WT" , "WU" , "WV" , "WW" , "WX" , "WY" , "WZ" , "XA" , "XB" , "XC" , "XD" , "XE" , "XF" , "XG" , "XH" , "XI" , "XJ" , "XK" , "XL" , "XM" , "XN" , "XO" , "XP" , "XQ" , "XR" , "XS" , "XT" , "XU" , "XV" , "XW" , "XX" , "XY" , "XZ" , "YA" , "YB" , "YC" , "YD" , "YE" , "YF" , "YG" , "YH" , "YI" , "YJ" , "YK" , "YL" , "YM" , "YN" , "YO" , "YP" , "YQ" , "YR" , "YS" , "YT" , "YU" , "YV" , "YW" , "YX" , "YY" , "YZ" , "ZA" , "ZB" , "ZC" , "ZD" , "ZE" , "ZF" , "ZG" , "ZH" , "ZI" , "ZJ" , "ZK" , "ZL" , "ZM" , "ZN" , "ZO" , "ZP" , "ZQ" , "ZR" , "ZS" , "ZT" , "ZU" , "ZV" , "ZW" , "ZX" , "ZY" , "ZZ"]
@@ -82,9 +90,6 @@ def nameCollumn(collumn):
 """ 
 
 
-for i in range(1):
-    print(nameCollumn(i))
-
 
 def generateSetBGColorString(ID):
     return f"\x1b[48;5;{ID}m"
@@ -93,13 +98,15 @@ def generateSetBGColorString(ID):
     #The proceeding 216 colors (16-231) or formed by a 3bpc RGB value offset by 16, packed into a single value.
     #The final 24 colors (232-255) are grayscale starting from a shade slighly lighter than black, ranging up to shade slightly darker than white.
 
+def generateSetTextColorString(ID):
+    return f"\x1b[38;5;{ID}m"
 
 
-def resetBGColor():
+def resetBGColor():                           #I think it doesn't work
+
     color = "253"
     sys.stdout.write(f"\x1b[48;5;{color}m")
     sys.stdout.flush()
-
 
 
 def invertDimentions(liste):
@@ -129,15 +136,15 @@ def invertDimentions(liste):
 
 
 
-"""printscreen([[9.123456789,2,3,4,5,6,6],
+printscreen([[9.123456789,2,3,4,5,6,6],
              [2,2,2,2,2,2,2],
              [9,8,7,6,6,5,4],
              [9,9,9,912345678901234567890,9,9,9],
              [1,1,1,1,2,2,2],
              [1,2,3,4,5,6,7],
-             [0,0,0,0,0,0,0]],0,0,15)
+             [0,0,0,0,0,0,0]],1,0,15)
 
-"""
+
     
     
     
