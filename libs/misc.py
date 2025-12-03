ab443a671017cbe3e5336ba7827b3258900004d7
PGM Productions <bonamimarius@gmail.com>
2025-12-03 09:27:21 +0100

    commit

---
+++ libs/misc.py
@@ -0,0 +1,9 @@
+#random useful fonctions
+
+
+def isNumber(string):
+    for char in string:
+        if not char in ("0","1","2","3","4","5","6","7","8","9"):
+            return False
+
+    return True
