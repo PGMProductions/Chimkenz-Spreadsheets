80c2aa43022b98c62c6fa661fcea009a5e6972db
PGM Productions <bonamimarius@gmail.com>
2025-12-11 11:39:24 +0100

    test

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from libs.printSpreadsheet import printScreen
 import csv
 import sys
+
+
+from libs.printSpreadsheet import printScreen
+from libs.calculations import Grid
+from libs.cursor import Cursor
+
 
 from cursor import Cursor
 
@@ -14,15 +19,54 @@ class ChimkenzSpreadsheet:
 		self.originY = 0
 
 		self.cursor = Cursor()
+		self.grid   = Grid()
 
-		self.spreadsheet = []
+		self.keepRunning = True
+
 		with open(filepath , newline = "") as csvfile:
 			spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
 			for row in spamreader:
-				self.spreadsheet.append(row)
+				for collumn in row:
+					grid.setSquare((row,collumn))
+
+		self.options = {}
+		#available options are
+			#autoUpdate : Bool
+			
+
+		with open("OPTIONS.txt" , newline = "") as csvfile:
+			spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+			for row in spamreader:
+				options[row[0]] = row[1]
+
+
+
+		self._launchMainLoop()
+
 
 	def updateScreen(self):
-		printScreen(self.spreadsheet,self.originX,self.originY,8,cursor.X(),cursor.Y())            #add collumn size choosing when options are done
+		"""updates what is shown on screen but not the values"""
+		printScreen(self.grid.getValueGrid(),self.originX,self.originY,8,cursor.X(),cursor.Y())            #add collumn size choosing when options are done
+
+
+	def _updateValues(self):
+		"""updates the values of the grid but not what is shown on screen"""
+		self.grid.update()
+
+
+
+	def _input(self):         #to do
+
+
+
+
+
+
+
+
+	def _launchMainLoop(self):
+		while self.keepRunning:
+
 
 
 
@@ -33,4 +77,4 @@ class ChimkenzSpreadsheet:
 
 
 if __name__ == "__main__":
-	chimkenzSpreadsheet("DIS_COM_UDI_2020.csv")
+	ChimkenzSpreadsheet("DIS_COM_UDI_2020.csv")
